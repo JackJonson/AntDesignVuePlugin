76d29b883f3dccf1b08f88d07ee4ac29fcb149e2
GitHub Action <action@github.com>
2022-04-03 03:11:19 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jackjonson/antdesignvueplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jackjonson.antdesignvueplugin.services
+
+import com.github.jackjonson.antdesignvueplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
